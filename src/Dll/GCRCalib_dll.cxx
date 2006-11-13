621557436aad7b5cb1e05f2260eb2102a11765d7
claval <>
2006-11-13 09:59:38 +0000

    New package: Selection of useful Xtals for calibration

---
+++ src/Dll/GCRCalib_dll.cxx
@@ -0,0 +1,3 @@
+// $Header: /nfs/slac/g/glast/ground/cvs/CalXtalResponse/src/Dll/CalXtalResponse_dll.cxx,v 1.2 2006/01/09 21:08:22 fewtrell Exp $
+#include "GaudiKernel/LoadFactoryEntries.h"
+LOAD_FACTORY_ENTRIES(GCRCalib)
