332110082d13d2c6ccdcceb14f8c0d0c43953a27
ecephas <>
2008-12-09 00:08:26 +0000

    Check in GCRCalibLib.py for GCRCalib v1r6p1 (branch-tag, GlastReleasev15r49).

--- GCRCalibLib.py
+++ GCRCalibLib.py
@@ -1,7 +1,11 @@
-# $Header$
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/GCRCalib/GCRCalibLib.py,v 1.1 2008/08/15 21:42:35 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['GCRCalib'])
-
+    env.Tool('addLibrary', library = env['rootLibs'])
+    env.Tool('addLibrary', library = env['rootGuiLibs'])
+    env.Tool('EventLib')
+    env.Tool('TkrUtilLib')
+    env.Tool('OnboardFilterTdsLib')
 def exists(env):
     return 1;
